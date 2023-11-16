633ba5a89346ac9f2c122d99497a5973b3c945f2
Kevin Lizarazu <sp1cybo1@vt.edu>
2023-11-16 12:41:10 -0500

    1) Made sliding animation for header

--- portfolio/home/static/home/js/home.js
+++ portfolio/home/static/home/js/home.js
@@ -1,27 +1,28 @@
-/* Scrolling Roles Implementation */
-let rolesField = document.getElementById("rotating-roles")
-const roles = [
-    "Bolivian",
-    "A Computer Engineer",
-    "An Entrepreneur",
-    "A Hokie",
-    "A Photographer",
-    "A Researcher",
-    "A Multi-Instrumentalist",
-    "A Web Developer",
-    "A Tennis Player",
-]
+// /* Scrolling Roles Implementation */
+// let rolesField = document.getElementById("rotating-roles")
+// const roles = [
+//     "Bolivian",
+//     "A Computer Engineer",
+//     "An Entrepreneur",
+//     "A Hokie",
+//     "A Photographer",
+//     "A Researcher",
+//     "A Multi-Instrumentalist",
+//     "A Web Developer",
+//     "A Tennis Player",
+//     "Software Developer",
+// ]
 
-const scrollAnimtation = {
-    transitionDuration: '3s',
-    transitionProperty: 'all',
-    transitionTimingFunction: 'ease'
-}
+// const scrollAnimtation = {
+//     transitionDuration: '3s',
+//     transitionProperty: 'all',
+//     transitionTimingFunction: 'ease'
+// }
 
-let currentIndex = 0;
-const rotateRoles = () => {
-    rolesField.textContent = roles[currentIndex];
-    currentIndex = (currentIndex + 1) % roles.length;
-}
+// let currentIndex = 0;
+// const rotateRoles = () => {
+//     rolesField.textContent = roles[currentIndex];
+//     currentIndex = (currentIndex + 1) % roles.length;
+// }
 
-setInterval(rotateRoles, 4000);
+// setInterval(rotateRoles, 4000);
