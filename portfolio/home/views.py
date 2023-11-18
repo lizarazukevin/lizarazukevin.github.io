350936d6ec7c806e492acc2eb4ecfc5a90ce2320
Kevin Lizarazu <sp1cybo1@vt.edu>
2023-11-17 22:09:02 -0500

    1) First iteration of education in home-about done

--- portfolio/home/views.py
+++ portfolio/home/views.py
@@ -5,14 +5,21 @@ from django.shortcuts import render
 from django.views import generic
 from django.http import HttpResponse
 
-from .models import Experience
+from .models import Experience, Education, Skill
 
 # Landing/Home page here
 class HomeView(generic.ListView):
     template_name = "home/home.html"
+    context_object_name = "home_list"
 
     def get_queryset(self):
         return HttpResponse("200")
+    
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['education_list'] = Education.objects.all()
+        context['skill_list'] = Skill.objects.all()
+        return context
 
 # Career page here
 class CareerView(generic.ListView):
