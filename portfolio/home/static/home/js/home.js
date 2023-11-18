18cae83adbba2a5355335ffff8a88451cf20f511
Kevin Lizarazu <sp1cybo1@vt.edu>
2023-11-17 19:08:43 -0500

    1) Added about me buttons 2) Fixed ugly bugs

--- portfolio/home/static/home/js/home.js
+++ portfolio/home/static/home/js/home.js
@@ -1,28 +1,55 @@
-// /* Scrolling Roles Implementation */
-// let rolesField = document.getElementById("rotating-roles")
-// const roles = [
-//     "Bolivian",
-//     "A Computer Engineer",
-//     "An Entrepreneur",
-//     "A Hokie",
-//     "A Photographer",
-//     "A Researcher",
-//     "A Multi-Instrumentalist",
-//     "A Web Developer",
-//     "A Tennis Player",
-//     "Software Developer",
-// ]
+/* About Navigation Button Functionality */
+let aboutSummaryButton = document.getElementById("about-summary-button");
+let aboutEducationButton = document.getElementById("about-education-button");
+let aboutSkillsButton = document.getElementById("about-skills-button");
 
-// const scrollAnimtation = {
-//     transitionDuration: '3s',
-//     transitionProperty: 'all',
-//     transitionTimingFunction: 'ease'
-// }
+let aboutSections = [".about-summary", ".about-education", ".about-skills"];
 
-// let currentIndex = 0;
-// const rotateRoles = () => {
-//     rolesField.textContent = roles[currentIndex];
-//     currentIndex = (currentIndex + 1) % roles.length;
-// }
+const clearActive = () => {
+    // clear any active buttons
+    let navItems = document.querySelectorAll(".about-nav-item");
+    for (let i = 0; i < navItems.length; i++) {
+        if (navItems[i].classList.contains("about-active")) {
+            navItems[i].classList.remove("about-active");
+            navItems[i].classList.add("about-unactive");
+        }
+    }
 
-// setInterval(rotateRoles, 4000);
+    // make all section displays be none
+    for (let i = 0; i < aboutSections.length; i++) {
+        let section = document.querySelector(aboutSections[i]);
+        section.style.display = "none";
+    }
+}
+
+const handleSummary = () => {
+    let content = document.querySelector(".about-summary");
+    let button = document.getElementById("about-summary-button");
+    content.style.display = "flex";
+    button.classList.add("about-active");
+}
+const handleEducation = () => {
+    let content = document.querySelector(".about-education");
+    let button = document.getElementById("about-education-button");
+    content.style.display = "flex";
+    button.classList.add("about-active");
+}
+const handleSkills = () => {
+    let content = document.querySelector(".about-skills");
+    let button = document.getElementById("about-skills-button");
+    content.style.display = "flex";
+    button.classList.add("about-active");
+}
+
+aboutSummaryButton.addEventListener('click', () => {
+    clearActive();
+    handleSummary();
+})
+aboutEducationButton.addEventListener('click', () => {
+    clearActive();
+    handleEducation();
+})
+aboutSkillsButton.addEventListener('click', () => {
+    clearActive();
+    handleSkills();
+})
